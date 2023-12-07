40dc5b5bc3e6d9aaa3cb61ab37c147a8959ac357
mohammed <mdtameem555@gmail.com>
2023-12-08 00:25:40 +0530

    proj added.

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oadcdive3JgdVt6C5d7',
+        issuer: 'https://dev-44172515.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
